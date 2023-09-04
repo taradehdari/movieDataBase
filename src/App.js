9aa67a8b0b5732c78f92d1da4b4c64a9b410b9e8
taradehdari <tara.dehdari@gmail.com>
2023-09-03 18:33:34 -0700

    added global styles to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+
+//styles
+import { GlobalStyle } from './GlobalStyle';
+
 function App() {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle />
     </div>
   );
 }
