26e414f46b5f01bd97bb9199afaf14c7bd1eaf62
taradehdari <tara.dehdari@gmail.com>
2023-09-05 13:10:24 -0700

    fetching data from API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+// API
+import API from '../API';
 
 // config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
-// components
+// Components
 
-// hook
+// Hook
 
-// image
+// Image
 import NoImage from '../images/no_image.jpg'
 
 const Home = () => {
@@ -14,6 +16,30 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     const[error, setError] = useState(false);
 
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+
+            setState(prev => ({
+                ...movies,
+                results:
+                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }));
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    };
+
+    // Initial render 
+    useEffect(() => {
+        fetchMovies(1)
+    }, []);
+
+    console.log(state);
 
     return <div>Home Page</div>
 }
