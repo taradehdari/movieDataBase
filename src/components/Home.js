356e7b6fe1d786bcaa060bceb10f375f36705361
taradehdari <tara.dehdari@gmail.com>
2023-09-12 18:37:43 -0700

    added loading button without style

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -45,7 +46,10 @@ const Home = () => {
                     />
                 ))}
             </Grid>
-            <Spinner />
+            {loading && <Spinner />}
+            {state.page < state.total_pages && !loading && (
+                <Button text='Load More' />
+            )}
         </>
     );
 };
